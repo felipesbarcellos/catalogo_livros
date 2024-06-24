e181a9dfcad7e52740a4646ed8409a94f0ea5482
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-06-24 19:32:44 -0300

    Added debuging with loguru

--- app/controllers.py
+++ app/controllers.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 from models import Author, Book
+from loguru import logger
 
 engine = create_engine("sqlite:///books.db", echo=True)
 
@@ -8,13 +9,13 @@ def select_all_books():
     session = Session(engine)
     stmt = select(Book).where()
     for book in session.scalars(stmt):
-        print(book)
+        logger.info(book)
 
 def select_all_authors():
     session = Session(engine)
     stmt = select(Author).where()
     for author in session.scalars(stmt):
-        print(author)
+        logger.info(author)
 
 def create_book(nome:str, authors: list[Author]):
     session = Session(engine)
@@ -25,5 +26,8 @@ def create_book(nome:str, authors: list[Author]):
     pass
 
 if __name__ == "__main__":
-    # select_all_books()
+    #create_book("A mulher de trinta anos", [Author(first_name = "Honoré de", last_name = "Balzac")])
+    #create_book("Quincas Borbas", [Author(first_name = "Machado de", last_name = "Assis")])
+    #select_all_books()
+    #select_all_authors()
     pass
