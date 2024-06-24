7451a0f2c659c3006cd566fa91597584aa83098a
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-06-24 18:55:54 -0300

    Created select all functions

--- app/controllers.py
+++ app/controllers.py
@@ -1,5 +1,25 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 from models import Author, Book
 
-engine = create_engine("sqlite://", echo=True)
+engine = create_engine("sqlite:///books.db", echo=True)
+
+def select_all_books():
+    session = Session(engine)
+    stmt = select(Book).where()
+    for book in session.scalars(stmt):
+        print(book)
+
+def select_all_authors():
+    session = Session(engine)
+    stmt = select(Author).where()
+    for author in session.scalars(stmt):
+        print(author)
+
+def create_book():
+    ...
+    pass
+
+if __name__ == "__main__":
+    # select_all_books()
+    pass
