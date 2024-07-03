b85b3387b8022e8648d714d3c9e11b0e1e260a19
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-07-03 06:26:33 -0300

    Modified engine from Controllers, now it uses Flask db variable

--- app/controllers.py
+++ app/controllers.py
@@ -2,32 +2,34 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 from models import Author, Book
 from loguru import logger
+from models import db
+from sqlalchemy import select
 
-engine = create_engine("sqlite:///books.db", echo=True)
-
+#Seleciona todos os livros
 def select_all_books():
-    session = Session(engine)
-    stmt = select(Book).where()
-    for book in session.scalars(stmt):
+    books = db.session.execute(select(Book))
+    for book in books:
         logger.info(book)
 
-def select_all_authors():
-    session = Session(engine)
-    stmt = select(Author).where()
-    for author in session.scalars(stmt):
-        logger.info(author)
+#Seleciona todos os autores
+#def select_all_authors():
+    #session = Session(engine)
+    #stmt = select(Author).where()
+    #for author in session.scalars(stmt):
+    #    logger.info(author)
 
-def create_book(nome:str, authors: list[Author]):
-    session = Session(engine)
-    book = Book(name=nome, authors=authors)
-    session.add(book)
-    session.commit()
-    ...
-    pass
+#Cria um novo livro
+#def create_book(nome:str, authors: list[Author]):
+ #   session = Session(engine)
+  #  book = Book(name=nome, authors=authors)
+   # session.add(book)
+    #session.commit()
+    #...
+    #pass
 
 if __name__ == "__main__":
     #create_book("A mulher de trinta anos", [Author(first_name = "Honoré de", last_name = "Balzac")])
     #create_book("Quincas Borbas", [Author(first_name = "Machado de", last_name = "Assis")])
-    #select_all_books()
+    select_all_books()
     #select_all_authors()
     pass
