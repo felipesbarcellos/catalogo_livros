d28d13c6bb3f9162efc40d81f9ef8b63c271a73b
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-07-03 06:23:21 -0300

    Added shelf model and added fields in many models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped
@@ -7,6 +8,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy import Column
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -21,6 +23,11 @@ class User(db.Model, UserMixin):
     username: Mapped[str] = mapped_column(nullable=False, unique=True)
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
     password: Mapped[str] = mapped_column(nullable=False)
+    reviews: Mapped[List["Review"]] = relationship()
+    bio: Mapped[str] = mapped_column(nullable=True)
+    location:Mapped[str] = mapped_column(nullable=True)
+    profile_picture:Mapped[str] = mapped_column(nullable=True)
+
 
     def __init__(self, username, email, password):
         self.username = username
@@ -30,23 +37,48 @@ class User(db.Model, UserMixin):
     def verify_password(self, psw):
         return check_password_hash(self.password, psw)
 
+class Review(db.Model):
+    __tablename__ = "reviews_table"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    usuario_id: Mapped[int] = mapped_column(ForeignKey("user_accounts.id"))
+    content: Mapped[str]
+    created_at: Mapped[datetime]
+
+    def __init__(self, usuario_id, content) -> None:
+        self.created_at = datetime.now()
+        self.usuario_id = usuario_id
+        self.content = content
+    def __repr__(self) -> str:
+        return f"Review(id={self.id!r}, content={self.content!r})"
+
+
+
+author_book_table = db.Table(
+    "author_book_table",
+    db.metadata,
+    Column("author_id", ForeignKey("authors_table.id")),
+    Column("book_id", ForeignKey("books_table.id")),
+)
 
 class Book(db.Model):
-    __tablename__ = "books"
+    __tablename__ = "books_table"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
-    authors: Mapped[List["Author"]] = relationship(
-        back_populates="books", cascade='all, delete-orphan'
-    )
+    isbn: Mapped[int]
+    reviews: Mapped[list[Review]] = relationship()
     def __repr__(self) -> str:
-        return f"Book(id={self.id!r}, name={self.name!r}), authors={self.authors!r})"
+        return f"Book(id={self.id!r}, name={self.name!r})"
 
 class Author(db.Model):
-    __tablename__ = "authors"
+    __tablename__ = "authors_table"
     id: Mapped[int] = mapped_column(primary_key=True)
     first_name: Mapped[str]
     last_name: Mapped[str]
-    book_id: Mapped[int] = mapped_column(ForeignKey("books.id"))
-    books: Mapped[List[Book]] = relationship(back_populates="authors")
+    livros: Mapped[list[Book]] = relationship(secondary=author_book_table)
     def __repr__(self) -> str:
         return f"Author(id={self.id!r}, first_name={self.first_name!r}, last_name={self.last_name!r})"
+
+class Shelf(db.Model):
+    __tablename__ = "shelf_table"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[User] = mapped_column(ForeignKey("user_accounts.id"))
